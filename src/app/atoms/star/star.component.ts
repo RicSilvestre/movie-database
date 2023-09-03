2957b175691fde6f9d22a2703c38e1f481530dc0
Ricardo <ricardomsilvestre@outlook.com>
2023-09-03 00:21:20 -0300

    [Rating] Implement precise rating, different star

--- src/app/atoms/star/star.component.ts
+++ src/app/atoms/star/star.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnChanges, HostBinding, SimpleChanges } from '@angular/core';
 
 @Component({
   selector: 'app-star',
   templateUrl: './star.component.html',
   styleUrls: ['./star.component.scss']
 })
-export class StarComponent {
+export class StarComponent implements OnChanges{
+  @Input() currentRating?: string = 'N/A';
+  @Input() gradientPct: string = '100%';
+  @Input() starType: string = "filled";
 
+  ngOnChanges(changes: SimpleChanges): void {
+    console.log(changes);
+  }
 }
